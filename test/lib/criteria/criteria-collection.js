73b17b3902a352f5a63b6b99992b6602efebfffe
Ilia Isupov <matumbaman@yandex-team.ru>
2015-12-13 13:16:04 +0300

    Added ability to pass multiple filter criteria

---
+++ test/lib/criteria/criteria-collection.js
@@ -0,0 +1,80 @@
+'use strict';
+
+var Criteria = require('../../../lib/criteria/criteria'),
+    CriteriaCollection = require('../../../lib/criteria/criteria-collection');
+
+describe('CriteriaCollection', function() {
+    describe('constructor', function() {
+        it('should add passed criteria', function() {
+            var criteria = new Criteria(),
+                collection = new CriteriaCollection(criteria);
+
+            assert.equal(criteria, collection.getFilterCriteria()[0]);
+        });
+
+        it('should accept criteria passed as array', function() {
+            var criteria = new Criteria(),
+                collection = new CriteriaCollection([criteria]);
+
+            assert.equal(criteria, collection.getFilterCriteria()[0]);
+        });
+    });
+
+    describe('addCriteria', function() {
+        it('should throw if passed something that is not a `Criteria` instance', function() {
+            var collection = new CriteriaCollection();
+
+            assert.throw(collection.addCriteria.bind(collection, {}));
+        });
+
+        it('should add criteria', function() {
+            var criteria = new Criteria(),
+                collection = new CriteriaCollection();
+
+            collection.addCriteria(criteria);
+
+            assert.deepEqual([criteria], collection.getFilterCriteria());
+        });
+    });
+
+    describe('getFilterCriteria', function() {
+        it('should return empty array if no criterias added to collection', function() {
+            var collection = new CriteriaCollection();
+
+            assert.deepEqual([], collection.getFilterCriteria());
+        });
+
+        it('should return single empty criteria if added multiple empty criteria and nothing else', function() {
+            var collection = new CriteriaCollection();
+
+            collection.addCriteria(new Criteria());
+            collection.addCriteria(new Criteria());
+
+            assert.deepEqual(collection.getFilterCriteria(), [new Criteria()]);
+        });
+
+        it('should remove all empty criteria if added at least 1 non-empty criteria', function() {
+            var nonEmptyCriteria = new Criteria({
+                    blocks: ['foo']
+                }),
+                collection = new CriteriaCollection(nonEmptyCriteria);
+
+            collection.addCriteria(new Criteria());
+            collection.addCriteria(new Criteria());
+
+            assert.deepEqual(collection.getFilterCriteria(), [nonEmptyCriteria]);
+        });
+
+        it('should return all non-empty criteria', function() {
+            var criteria = new Criteria({
+                    blocks: ['foo']
+                }),
+                anotherCriteria = new Criteria({
+                    blocks: ['bar']
+                }),
+                collection = new CriteriaCollection([criteria, anotherCriteria]);
+
+            assert.deepEqual(collection.getFilterCriteria(), [criteria, anotherCriteria]);
+        });
+    });
+});
---
+++ test/lib/criteria/criteria.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var Criteria = require('../../../lib/criteria/criteria');
+
+describe('criteria', function() {
+    describe('constructor', function() {
+        testFieldInit_('blocks');
+        testFieldInit_('elements');
+        testFieldInit_('modifiers');
+        testFieldInit_('techs');
+    });
+
+    describe('isEmpty', function() {
+        it('should return true if criteria has no defined conditions', function() {
+            var criteria = new Criteria({});
+
+            assert.equal(criteria.isEmpty(), true);
+        });
+
+        it('should return false if criteria has at least 1 defined condition', function() {
+            var criteria = new Criteria({
+                blocks: ['foo']
+            });
+
+            assert.equal(criteria.isEmpty(), false);
+        });
+    });
+
+    describe('isEqual', function() {
+        it('should return false if comparing with something that is not a Criteria instance', function() {
+            var criteria = new Criteria();
+
+            assert.equal(criteria.isEqual({}), false);
+        });
+
+        it('should return false if at least one of conditions differs between 2 criterias', function() {
+            var criteria = new Criteria({blocks: ['foo']}),
+                another = new Criteria({blocks: ['bar']});
+
+            assert.equal(criteria.isEqual(another), false);
+        });
+
+        it('should return true if both criterias has same conditions', function() {
+            var criteria = new Criteria({blocks: ['foo']}),
+                another = new Criteria({blocks: ['foo']});
+
+            assert.equal(criteria.isEqual(another), true);
+        });
+    });
+});
+
+function testFieldInit_(fieldName) {
+    it('should set field' + fieldName + 'if it passed', function() {
+        var opts = {};
+        opts[fieldName] = ['foo'];
+
+        var criteria = new Criteria(opts);
+
+        assert.deepEqual(criteria[fieldName], ['foo']);
+    });
+
+    it('should set field' + fieldName + 'as empty array if it was not passed', function() {
+        var opts = {
+            blocks: ['foo'],
+            elements: ['bar'],
+            modifiers: ['fizz'],
+            techs: ['baz']
+        };
+
+        delete opts[fieldName];
+
+        var criteria = new Criteria(opts);
+
+        assert.deepEqual(criteria[fieldName], []);
+    });
+}
