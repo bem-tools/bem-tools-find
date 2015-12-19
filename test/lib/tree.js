3b71566bd89e6843630ba33c5ca309db8064acd2
tormozz48 <tormozz48@gmail.com>
2015-12-20 00:44:34 +0300

    Add tests for tree module

---
+++ test/lib/tree.js
@@ -0,0 +1,185 @@
+'use strict';
+
+var _ = require('lodash'),
+    Tree = require('../../lib/tree');
+
+describe('Tree', function() {
+    var tree,
+        levelItem = {level: 'some-level'},
+        blockItem = _.extend({}, levelItem, {
+            block: 'some-block',
+            tech: 'some-tech',
+            path: 'some-path'
+        }),
+        blockModeItem = _.extend({}, levelItem, blockItem, {
+            modName: 'some-mod-name',
+            modVal: 'some-mod-val'
+        }),
+        elemItem = _.extend({}, levelItem, blockItem, {
+            elem: 'some-elem'
+        }),
+        elemModeItem = _.extend({}, levelItem, blockItem, elemItem, {
+            modName: 'some-elem-mod-name',
+            modVal: 'some-elem-mod-val'
+        });
+
+    function getChild(order) {
+        var result = tree.getTree();
+        for (var i = 1; i <= order; i++) {
+            result = result.nodes[0];
+        }
+        return result;
+    }
+
+    beforeEach(function() {
+        tree = new Tree('some-title');
+    });
+
+    it('should create tree with given title', function() {
+        assert.equal(tree.getTree().label, 'some-title');
+    });
+
+    it('should create tree with empty nodes array', function() {
+        assert.lengthOf(tree.getTree().nodes, 0);
+    });
+
+    describe('addLevelNode', function() {
+        it('should add level node if it does not exist yet', function() {
+            tree.addLevelNode(levelItem);
+            assert.deepEqual(getChild(1), {label: 'some-level', nodes: []});
+        });
+
+        it('should not add level nodes with same labels multiple times', function() {
+            tree.addLevelNode(levelItem);
+            tree.addLevelNode(levelItem);
+
+            assert.lengthOf(tree.getTree().nodes, 1);
+        });
+
+        it('should return founded or created level node', function() {
+            assert.deepEqual(tree.addLevelNode(levelItem), {label: 'some-level', nodes: []});
+        });
+    });
+
+    describe('addBlockNode', function() {
+        var treeItem = {label: 'some-block', nodes: [], leaf: {'some-tech': 'some-path'}};
+
+        it('should add block node if it does not exist yet', function() {
+            tree.addBlockNode(blockItem, true);
+            assert.deepEqual(getChild(2), treeItem);
+        });
+
+        it('should not add leaf if corresponded flag is equal to false', function() {
+            tree.addBlockNode(blockItem, false);
+            assert.deepEqual(getChild(2), _.omit(treeItem, 'leaf'));
+        });
+
+        it('should not add block nodes with same labels multiple times', function() {
+            tree.addBlockNode(blockItem, true);
+            tree.addBlockNode(blockItem, true);
+
+            assert.lengthOf(getChild(1).nodes, 1);
+        });
+
+        it('should return founded or created block node', function() {
+            assert.deepEqual(tree.addBlockNode(blockItem, true), treeItem);
+        });
+    });
+
+    describe('addModeNode', function() {
+        var treeItem = {
+                label: '_some-mod-name',
+                nodes: [
+                    {
+                        label: 'some-mod-val',
+                        nodes: [],
+                        leaf: {'some-tech': 'some-path'}
+                    }
+                ]
+            };
+
+        it('should add block mode node if it does not exist yet', function() {
+            tree.addModeNode(blockModeItem, true);
+            assert.deepEqual(getChild(3), treeItem);
+        });
+
+        it('should not add leaf if corresponded flag is equal to false', function() {
+            tree.addModeNode(blockModeItem, false);
+            assert.deepEqual(getChild(4), _.omit(treeItem.nodes[0], 'leaf'));
+        });
+
+        it('should not add block mode nodes with same labels multiple times', function() {
+            tree.addModeNode(blockModeItem, true);
+            tree.addModeNode(blockModeItem, true);
+
+            assert.lengthOf(getChild(2).nodes, 1);
+        });
+
+        it('should return founded or created block mode node', function() {
+            assert(tree.addModeNode(blockModeItem, true), treeItem);
+        });
+    });
+
+    describe('addElemNode', function() {
+        var treeItem = {
+            label: '__some-elem',
+            nodes: [],
+            leaf: {'some-tech': 'some-path'}
+        };
+
+        it('should add elem node if it does not exist yet', function() {
+            tree.addElemNode(elemItem, true);
+            assert.deepEqual(getChild(3), treeItem);
+        });
+
+        it('should not add leaf if corresponded flag is equal to false', function() {
+            tree.addElemNode(elemItem, false);
+            assert.deepEqual(getChild(3), _.omit(treeItem, 'leaf'));
+        });
+
+        it('should not add elem nodes with same labels multiple times', function() {
+            tree.addElemNode(elemItem, true);
+            tree.addElemNode(elemItem, true);
+
+            assert.lengthOf(getChild(2).nodes, 1);
+        });
+
+        it('should return founded or created elem node', function() {
+            assert(tree.addElemNode(elemItem, true), treeItem);
+        });
+    });
+
+    describe('addElemModeNode', function() {
+        var treeItem = {
+            label: '_some-elem-mod-name',
+            nodes: [
+                {
+                    label: 'some-elem-mod-val',
+                    nodes: [],
+                    leaf: {'some-tech': 'some-path'}
+                }
+            ]
+        };
+
+        it('should add elem mode node if it does not exist yet', function() {
+            tree.addElemModeNode(elemModeItem, true);
+            assert.deepEqual(getChild(4), treeItem);
+        });
+
+        it('should not add leaf if corresponded flag is equal to false', function() {
+            tree.addElemModeNode(elemModeItem, false);
+            assert.deepEqual(getChild(5), _.omit(treeItem.nodes[0], 'leaf'));
+        });
+
+        it('should not add elem mode nodes with same labels multiple times', function() {
+            tree.addElemModeNode(elemModeItem, true);
+            tree.addElemModeNode(elemModeItem, true);
+
+            assert.lengthOf(getChild(3).nodes, 1);
+        });
+
+        it('should return founded or created elem mode node', function() {
+            assert(tree.addElemModeNode(elemModeItem, true), treeItem);
+        });
+    });
+});
